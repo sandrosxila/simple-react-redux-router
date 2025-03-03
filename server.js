c60686f8a32d289f5e9ebae3a476352aa46656c7
sandrosxila <sandrosxila@yahoo.com>
2025-03-03 16:55:38 +0400

    firebase server admin

--- server.js
+++ server.js
@@ -1,59 +1,37 @@
 import express, { json } from 'express';
 import cors from 'cors';
-import { v4 as uuidv4 } from 'uuid';
+import admin from "firebase-admin";
+
+import serviceAccount from './serviceAccount.json' with { type: 'json' }
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 const app = express();
 app.use(cors());
 app.use(json());
 
-let users = []; // In-memory storage
-
-// **Signup (Register)**
-app.post('/signup', (req, res) => {
-  const { email, password, fullName } = req.body;
-
-  if (!email || !password || !fullName) {
-    return res.status(400).json({ error: 'Email, password, and full name are required' });
-  }
-
-  if (users.some(user => user.email === email)) {
-    return res.status(409).json({ error: 'User with this email already exists' });
-  }
-
-  const newUser = { id: uuidv4(), email, password, fullName };
-  users.push(newUser);
-  res.status(201).json({ message: 'User registered successfully', userId: newUser.id });
-});
-
-// **Login**
-app.post('/signin', (req, res) => {
-  const { email, password } = req.body;
-
-  const user = users.find(user => user.email === email && user.password === password);
-  if (!user) {
-    return res.status(401).json({ error: 'Invalid email or password' });
-  }
-
-  res.json({ message: 'Login successful', userId: user.id, fullName: user.fullName });
+// **Get Users (For Debugging)**
+app.get('/users', async (req, res) => {
+  const results = await admin.auth().listUsers(1000);
+  res.json(results.users.map((user) => ({ id: user.uid, fullName: user.displayName, email: user.email })));
 });
 
 // **Get Users (For Debugging)**
-app.get('/users', (req, res) => {
-  // eslint-disable-next-line no-unused-vars
-  res.json(users.map(({password, ...user}) => user));
-});
-
-// **Get Users (For Debugging)**
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
   const { id } = req.params;
 
-  const user = users.find(user => id === user.id);
+  try {
+    const result = await admin.auth().getUsers([{uid: id}]);
 
-  if (!user) {
+    const user = result.users[0];
+
+    return res.json({ userId: user.uid, fullName: user.displayName, email: user.email });
+  }
+  catch {
     return res.status(404).json({ error: 'User not found' });
   }
-
-  res.json({ userId: user.id, fullName: user.fullName, email: user.email });
 });
 
 // **Start Server**
