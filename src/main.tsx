7e842dd8530b81d3c74a4d5667ef084520ef86c3
sandrosxila <sandrosxila@yahoo.com>
2025-03-10 17:22:30 +0400

    redux-persist

--- src/main.tsx
+++ src/main.tsx
@@ -8,18 +8,24 @@ import axios from "axios";
 import App from "./App.js";
 import { BrowserRouter } from "react-router";
 import { store } from "./store.js";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
 axios.defaults.baseURL = "http://localhost:5000";
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")).render(
+const persistor = persistStore(store);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </PersistGate>
       </Provider>
     </QueryClientProvider>
   </StrictMode>
--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import authReducer from '@/features/auth/user';
+import storage from 'redux-persist/lib/storage';
+import persistReducer from 'redux-persist/es/persistReducer';
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const reducer = combineReducers({
+  auth: authReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      }
+    })
+  
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
